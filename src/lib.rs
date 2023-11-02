702c5c9e4d1427a681777419211301b504fdb44a
AlexKnauth <alexander@knauth.org>
2023-11-02 16:00:33 -0400

    Set a panic hook if use_std

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,10 @@ const PROCESS_NAMES: [&str; 6] = [
 // --------------------------------------------------------
 
 async fn main() {
+    std::panic::set_hook(Box::new(|panic_info| {
+        asr::print_message(&panic_info.to_string());
+    }));
+
     // TODO: Set up some general state and settings.
 
     asr::print_message("Hello, World!");
