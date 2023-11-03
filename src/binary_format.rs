59cd771809d89bed4037b520011512d731af13da
AlexKnauth <alexander@knauth.org>
2023-11-03 19:45:36 -0400

    Tweak bytes_detect_binary_format

--- src/binary_format.rs
+++ src/binary_format.rs
@@ -49,13 +49,10 @@ fn bytes_detect_binary_format(bytes: &[u8]) -> Option<BinaryFormat> {
         Some(BinaryFormat::PE)
     } else if bytes.starts_with(&[0x7F, 0x45, 0x4C, 0x46]) {
         Some(BinaryFormat::ELF)
-    } else if bytes.starts_with(&[0xFE, 0xED, 0xFA, 0xCE]) {
-        Some(BinaryFormat::MachO)
-    } else if bytes.starts_with(&[0xFE, 0xED, 0xFA, 0xCF]) {
-        Some(BinaryFormat::MachO)
-    } else if bytes.starts_with(&[0xCE, 0xFA, 0xED, 0xFE]) {
-        Some(BinaryFormat::MachO)
-    } else if bytes.starts_with(&[0xCF, 0xFA, 0xED, 0xFE]) {
+    } else if bytes.starts_with(&[0xFE, 0xED, 0xFA, 0xCE])
+           || bytes.starts_with(&[0xFE, 0xED, 0xFA, 0xCF])
+           || bytes.starts_with(&[0xCE, 0xFA, 0xED, 0xFE])
+           || bytes.starts_with(&[0xCF, 0xFA, 0xED, 0xFE]) {
         Some(BinaryFormat::MachO)
     } else {
         None
