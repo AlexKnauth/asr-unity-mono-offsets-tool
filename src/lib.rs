9c2ac2d6e9d56bfad626f93ca5cb78fabec2336b
Alex Knauth <alexander@knauth.org>
2024-01-27 22:10:54 -0500

    monoclass_name 32-bit cattrs version?

--- src/lib.rs
+++ src/lib.rs
@@ -350,7 +350,7 @@ async fn option_main(process: &Process) -> Option<()> {
     // But monoclassdef_klass seems to always be 0 anyway.
     let monoclassdef_klass = 0x0;
     asr::print_message(&format!("Offsets monoclassdef_klass: 0x{:X?}, ASSUMED", monoclassdef_klass));
-    let (monoclass_name, monoclass_name_space) = [(0x2C, 0x30), (0x30, 0x34), (0x38, 0x40), (0x40, 0x48), (0x48, 0x50)].into_iter().max_by_key(|&(monoclass_name, monoclass_name_space)| {
+    let (monoclass_name, monoclass_name_space) = [(0x2C, 0x30), (0x30, 0x34), (0x34, 0x38), (0x38, 0x40), (0x40, 0x48), (0x48, 0x50)].into_iter().max_by_key(|&(monoclass_name, monoclass_name_space)| {
         let class_name_score = monoclass_name_score(process, deref_type, class, monoclassdef_klass, monoclass_name, monoclass_name_space);
         // asr::print_message(&format!("monoclass_name: 0x{:X?} space: 0x{:X?}, class_name_score: {} / 9", monoclass_name, monoclass_name_space, class_name_score));
         class_name_score
